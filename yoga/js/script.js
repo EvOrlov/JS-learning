defac228e9bf2cada23ba62a7caccd78314a036e
Evgeniy <seaman2774@yandex.ru>
2021-11-26 14:28:05 +0300

    intermediate save

---
+++ yoga/js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("DOMContentLoaded", function() {
+
+    'use strict';
+    let infoHeaders = document.getElementsByClassName('info-header-tab'),
+        infoHeader = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent.fade');
+    console.log(tabContent);
+
+});
